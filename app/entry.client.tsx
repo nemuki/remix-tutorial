7acf92b11aae88664b82e01ea3c3d3faa09b277f
Naoki Takahashi <nemuki@users.noreply.github.com>
2023-01-07 14:32:57 +0900

    https://mantine.dev/guides/remix/ に沿って対象ファイルを変更

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,22 +1,10 @@
-import { RemixBrowser } from "@remix-run/react";
-import { startTransition, StrictMode } from "react";
-import { hydrateRoot } from "react-dom/client";
+import {RemixBrowser} from '@remix-run/react';
+import {hydrate} from 'react-dom';
+import {ClientProvider} from '@mantine/remix';
 
-function hydrate() {
-  startTransition(() => {
-    hydrateRoot(
-      document,
-      <StrictMode>
-        <RemixBrowser />
-      </StrictMode>
-    );
-  });
-}
-
-if (typeof requestIdleCallback === "function") {
-  requestIdleCallback(hydrate);
-} else {
-  // Safari doesn't support requestIdleCallback
-  // https://caniuse.com/requestidlecallback
-  setTimeout(hydrate, 1);
-}
+hydrate(
+    <ClientProvider>
+        <RemixBrowser/>
+    </ClientProvider>,
+    document
+);
